4655c2e37a2306647130a90419997ff8bf4026fc
lishangjie <53826293+lishangjie1@users.noreply.github.com>
2024-01-15 04:26:22 -0800

    Update interactive.sh

--- interactive.sh
+++ interactive.sh
@@ -1,17 +1,17 @@
 
 set -e
 
-model_path="/mnt/nas/users/lsj/music/models/llama_7b_sft_ast/checkpoint-18000"
-pretrain_model_path="/mnt/nas/users/lsj/llm/pretrained_model/llama_7b"
+model_path="/mnt/nas/users/lsj/music/models/llama2_7b_chat_ast_lora_s2_max100_only_open"
+# pretrain_model_path="/mnt/nas/users/lsj/llm/pretrained_model/llama_7b"
 # audio_encoder_path="/mnt/nas/users/lsj/music/models/whisper-large-v2"
-audio_encoder_path="/mnt/nas/users/lsj/music/models/AST"
+# audio_encoder_path="/mnt/nas/users/lsj/music/models/AST"
 # copy tokenizer
-cp $pretrain_model_path/tokenizer* $model_path/
+# cp $pretrain_model_path/tokenizer* $model_path/
 # copy preprocessor_config
-cp $audio_encoder_path/preprocessor_config.json $model_path/
+# cp $audio_encoder_path/preprocessor_config.json $model_path/
 # change vocab_size in config.json
-
+export CUDA_VISIBLE_DEVICES=1
 python blsp/interactive.py \
-    --audio_file /mnt/nas/users/lsj/music/blsp/0ONdm4sW47c.wav \
+    --audio_file /mnt/nas/users/lsj/music/blsp/00M9FhCet6s.wav \
     --blsp_model $model_path \
     --audio_model_type "ast"
